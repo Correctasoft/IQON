1270ce01f84f011ede48ca16f6aeec43b856147a
sadek al mostakim <s.a.mostakim@gmail.com>
2021-04-16 01:47:09 +0600

    1264515

--- routes/APIController/admin/product.js
+++ routes/APIController/admin/product.js
@@ -154,129 +154,183 @@ router.get("/", (req, res) => {
 });
 
 router.put("/", (req, res) => {
-  let base64Image = req.body.ProductImage.split(';base64,').pop();
-  let image1type = req.body.ProductImage.split(';base64,')[0];
-  let image2type = req.body.ProductImage1.split(';base64,')[0];
-  fs.writeFile('image.png', base64Image, {
-    encoding: 'base64'
-  }, async function (err) {
-    const image = await Jimp.read('image.png');
-    await image.resize(800, 800);
-    await image.quality(70);
-    await image.writeAsync('image.png');
-    let img1 = fs.readFileSync('image.png');
-    let encode_image1 = img1.toString('base64');
-    let base64Image2 = req.body.ProductImage1.split(';base64,').pop();
-    fs.writeFile('image.png', base64Image2, {
+  if (req.body.ProductImage.includes(';base64,') && req.body.ProductImage1.includes(';base64,')) {
+    let base64Image = req.body.ProductImage.split(';base64,').pop();
+    let image1type = req.body.ProductImage.split(';base64,')[0];
+    let image2type = req.body.ProductImage1.split(';base64,')[0];
+    fs.writeFile('image.png', base64Image, {
       encoding: 'base64'
     }, async function (err) {
-      const image2 = await Jimp.read('image.png');
-      await image2.resize(800, 800);
-      await image2.quality(70);
-      await image2.writeAsync('image.png');
-      let img2 = fs.readFileSync('image.png');
-      let encode_image2 = img2.toString('base64');
-      ProductModel.updateOne({
-          _id: req.body.Product_id,
-        }, {
-          $set: {
-            Name: req.body.ProductName,
-            Code: req.body.ProductCode,
-            Active: req.body.ProductActive,
-            Description: req.body.ProductDescription,
-            SellingPrice: req.body.ProductSellingPrice,
-            DiscountedPrice: req.body.ProductDiscountedPrice,
-            StockAvailable: req.body.ProductStockAvailable,
-            MainImage: image1type + ';base64,' + encode_image1,
-            SecondaryImage: image2type + ';base64,' + encode_image2,
-            IsFeatured: req.body.ProductFeatured,
-            Category: req.body.ProductCategory,
-            Size: req.body.ProductSize,
-            Color: req.body.ProductColor,
-            Tags: req.body.ProductTags,
-            UpdationDate: Date.now(),
-            //UpdatedBy: req.user._id
-          },
-        })
-        .then((_) => {
-          fs.unlink('image.png', (err => {
-            if (err) console.log(err);
-            else {
-              res.writeHead(200, {
-                "Content-Type": "image/png",
-                "Content-Length": img.length,
-              });
-              res.end(img);
-            }
-          }));
-          res.json({
-            status: 200,
-          });
-        })
-        .catch((err) => console.log(err));
+      const image = await Jimp.read('image.png');
+      await image.resize(800, 800);
+      await image.quality(70);
+      await image.writeAsync('image.png');
+      let img1 = fs.readFileSync('image.png');
+      let encode_image1 = img1.toString('base64');
+      let base64Image2 = req.body.ProductImage1.split(';base64,').pop();
+      fs.writeFile('image.png', base64Image2, {
+        encoding: 'base64'
+      }, async function (err) {
+        const image2 = await Jimp.read('image.png');
+        await image2.resize(800, 800);
+        await image2.quality(70);
+        await image2.writeAsync('image.png');
+        let img2 = fs.readFileSync('image.png');
+        let encode_image2 = img2.toString('base64');
+        ProductModel.updateOne({
+            _id: req.body.Product_id,
+          }, {
+            $set: {
+              Name: req.body.ProductName,
+              Code: req.body.ProductCode,
+              Active: req.body.ProductActive,
+              Description: req.body.ProductDescription,
+              SellingPrice: req.body.ProductSellingPrice,
+              DiscountedPrice: req.body.ProductDiscountedPrice,
+              StockAvailable: req.body.ProductStockAvailable,
+              MainImage: image1type + ';base64,' + encode_image1,
+              SecondaryImage: image2type + ';base64,' + encode_image2,
+              IsFeatured: req.body.ProductFeatured,
+              Category: req.body.ProductCategory,
+              Size: req.body.ProductSize,
+              Color: req.body.ProductColor,
+              Tags: req.body.ProductTags,
+              UpdationDate: Date.now(),
+              //UpdatedBy: req.user._id
+            },
+          })
+          .then((_) => {
+            fs.unlink('image.png', (err => {
+              if (err) console.log(err);
+              else {
+                res.writeHead(200, {
+                  "Content-Type": "image/png",
+                  "Content-Length": img.length,
+                });
+                res.end(img);
+              }
+            }));
+            res.json({
+              status: 200,
+            });
+          })
+          .catch((err) => console.log(err));
+      })
     })
-  })
+  } else {
+    ProductModel.updateOne({
+        _id: req.body.Product_id,
+      }, {
+        $set: {
+          Name: req.body.ProductName,
+          Code: req.body.ProductCode,
+          Active: req.body.ProductActive,
+          Description: req.body.ProductDescription,
+          SellingPrice: req.body.ProductSellingPrice,
+          DiscountedPrice: req.body.ProductDiscountedPrice,
+          StockAvailable: req.body.ProductStockAvailable,
+          IsFeatured: req.body.ProductFeatured,
+          Category: req.body.ProductCategory,
+          Size: req.body.ProductSize,
+          Color: req.body.ProductColor,
+          Tags: req.body.ProductTags,
+          UpdationDate: Date.now(),
+          //UpdatedBy: req.user._id
+        },
+      })
+      .then((_) => {
+        res.json({
+          status: 200,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
 });
 
 router.post("/", async (req, res) => {
-  let base64Image = req.body.ProductImage.split(';base64,').pop();
-  let image1type = req.body.ProductImage.split(';base64,')[0];
-  let image2type = req.body.ProductImage1.split(';base64,')[0];
-  fs.writeFile('image.png', base64Image, {
-    encoding: 'base64'
-  }, async function (err) {
-    const image = await Jimp.read('image.png');
-    await image.resize(500, 500);
-    await image.quality(70);
-    await image.writeAsync('image.png');
-    let img1 = fs.readFileSync('image.png');
-    let encode_image1 = img1.toString('base64');
-    let base64Image2 = req.body.ProductImage1.split(';base64,').pop();
-    fs.writeFile('image.png', base64Image2, {
+  if (req.body.ProductImage.includes(';base64,') && req.body.ProductImage.includes(';base64,')) {
+    let base64Image = req.body.ProductImage.split(';base64,').pop();
+    let image1type = req.body.ProductImage.split(';base64,')[0];
+    let image2type = req.body.ProductImage1.split(';base64,')[0];
+    fs.writeFile('image.png', base64Image, {
       encoding: 'base64'
     }, async function (err) {
-      const image2 = await Jimp.read('image.png');
-      await image2.resize(500, 500);
-      await image2.quality(70);
-      await image2.writeAsync('image.png');
-      let img2 = fs.readFileSync('image.png');
-      let encode_image2 = img2.toString('base64');
-      const newProduct = new ProductModel({
-        Name: req.body.ProductName,
-        Code: req.body.ProductCode,
-        Active: req.body.ProductActive,
-        Description: req.body.ProductDescription,
-        SellingPrice: req.body.ProductSellingPrice,
-        DiscountedPrice: req.body.ProductDiscountedPrice,
-        StockAvailable: req.body.ProductStockAvailable,
-        MainImage: image1type + ';base64,' + encode_image1,
-        SecondaryImage: image2type + ';base64,' + encode_image2,
-        IsFeatured: req.body.ProductFeatured,
-        Category: req.body.ProductCategory,
-        Size: req.body.ProductSize,
-        Color: req.body.ProductColor,
-        Tags: req.body.ProductTags,
-        InsertionDate: Date.now(),
-        //InsertedBy: req.user._id,
-        UpdationDate: Date.now(),
-        //UpdatedBy: req.user._id
-      });
-      await newProduct.save();
-      fs.unlink('image.png', (err => {
-        if (err) console.log(err);
-        else {
-          res.writeHead(200, {
-            "Content-Type": "image/png",
-            "Content-Length": img.length,
-          });
-          res.end(img);
-        }
-      }));
-      res.json({
-        status: 200,
+      const image = await Jimp.read('image.png');
+      await image.resize(500, 500);
+      await image.quality(70);
+      await image.writeAsync('image.png');
+      let img1 = fs.readFileSync('image.png');
+      let encode_image1 = img1.toString('base64');
+      let base64Image2 = req.body.ProductImage1.split(';base64,').pop();
+      fs.writeFile('image.png', base64Image2, {
+        encoding: 'base64'
+      }, async function (err) {
+        const image2 = await Jimp.read('image.png');
+        await image2.resize(500, 500);
+        await image2.quality(70);
+        await image2.writeAsync('image.png');
+        let img2 = fs.readFileSync('image.png');
+        let encode_image2 = img2.toString('base64');
+        const newProduct = new ProductModel({
+          Name: req.body.ProductName,
+          Code: req.body.ProductCode,
+          Active: req.body.ProductActive,
+          Description: req.body.ProductDescription,
+          SellingPrice: req.body.ProductSellingPrice,
+          DiscountedPrice: req.body.ProductDiscountedPrice,
+          StockAvailable: req.body.ProductStockAvailable,
+          MainImage: image1type + ';base64,' + encode_image1,
+          SecondaryImage: image2type + ';base64,' + encode_image2,
+          IsFeatured: req.body.ProductFeatured,
+          Category: req.body.ProductCategory,
+          Size: req.body.ProductSize,
+          Color: req.body.ProductColor,
+          Tags: req.body.ProductTags,
+          InsertionDate: Date.now(),
+          //InsertedBy: req.user._id,
+          UpdationDate: Date.now(),
+          //UpdatedBy: req.user._id
+        });
+        await newProduct.save();
+        fs.unlink('image.png', (err => {
+          if (err) console.log(err);
+          else {
+            res.writeHead(200, {
+              "Content-Type": "image/png",
+              "Content-Length": img.length,
+            });
+            res.end(img);
+          }
+        }));
+        res.json({
+          status: 200,
+        });
       });
     });
-  });
+  } else {
+    const newProduct = new ProductModel({
+      Name: req.body.ProductName,
+      Code: req.body.ProductCode,
+      Active: req.body.ProductActive,
+      Description: req.body.ProductDescription,
+      SellingPrice: req.body.ProductSellingPrice,
+      DiscountedPrice: req.body.ProductDiscountedPrice,
+      StockAvailable: req.body.ProductStockAvailable,
+      IsFeatured: req.body.ProductFeatured,
+      Category: req.body.ProductCategory,
+      Size: req.body.ProductSize,
+      Color: req.body.ProductColor,
+      Tags: req.body.ProductTags,
+      InsertionDate: Date.now(),
+      //InsertedBy: req.user._id,
+      UpdationDate: Date.now(),
+      //UpdatedBy: req.user._id
+    });
+    await newProduct.save();
+    res.json({
+      status: 200,
+    });
+  }
 });
 
 router.delete("/:id", (req, res) => {
