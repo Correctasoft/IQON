8f27b57eb523c3d768b3b7c68e50e75b7ba4cc74
sadek al mostakim <s.a.mostakim@gmail.com>
2021-04-17 16:17:05 +0600

    produt upload

--- routes/APIController/admin/product.js
+++ routes/APIController/admin/product.js
@@ -253,7 +253,7 @@ router.put("/", (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  if (req.body.ProductImage.includes(';base64,') && req.body.ProductImage.includes(';base64,')) {
+  if (req.body.ProductImage1 && req.body.ProductImage && req.body.ProductImage.includes(';base64,') && req.body.ProductImage1.includes(';base64,')) {
     let base64Image = req.body.ProductImage.split(';base64,').pop();
     let image1type = req.body.ProductImage.split(';base64,')[0];
     let image2type = req.body.ProductImage1.split(';base64,')[0];
